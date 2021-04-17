595eeae974c6b520f4f3c7cd0646fb6b138bc84f
skundu42 <skundu_be19@thapar.edu>
2021-04-18 00:36:54 +0530

    Updates

---
+++ precache-manifest.7c9813396dcaa773230ed1c8f6096c98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "229752e1082d71a765a8",
+    "url": "/Revoir/static/css/main.402900d8.chunk.css"
+  },
+  {
+    "revision": "229752e1082d71a765a8",
+    "url": "/Revoir/static/js/main.229752e1.chunk.js"
+  },
+  {
+    "revision": "f90cd3fbc010a8dec52c",
+    "url": "/Revoir/static/css/1.a8ec221e.chunk.css"
+  },
+  {
+    "revision": "f90cd3fbc010a8dec52c",
+    "url": "/Revoir/static/js/1.f90cd3fb.chunk.js"
+  },
+  {
+    "revision": "54aca0bdae310dc80095",
+    "url": "/Revoir/static/js/runtime~main.54aca0bd.js"
+  },
+  {
+    "revision": "d6cbd8e6d17d679dd3cd7fe3884ac8bb",
+    "url": "/Revoir/static/media/photo.d6cbd8e6.png"
+  },
+  {
+    "revision": "040736d79cc34d4c2ad9ad19571d5fd6",
+    "url": "/Revoir/index.html"
+  }
+];
